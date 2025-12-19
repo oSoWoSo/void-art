f6fd742a3cf6d0f7b9ec2343fa2e7e8d5b8c4d5d
zenobit <zenobit@disroot.org>
2025-12-19 02:14:58 +0100

    Fix page structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-let wallDiv = document.querySelector(".wallDiv");
-let wallPath = "assets/walls/";
+let hiDiv = document.querySelector(".hiDiv");
+let hiPath = "assets/wallpapers-hires/";
+let lowDiv = document.querySelector(".lowDiv");
+let lowPath = "assets/wallpapers-lowres/";
 let logoDiv = document.querySelector(".logoDiv");
 let logoPath = "assets/logos/";
+let grubDiv = document.querySelector(".grubDiv");
+let grubPath = "assets/grub/inkspace-files/";
 
 addImages = (n, path, mainDiv) => {
   for (let i = 1; i <= n; i++) {
     const image = document.createElement("img");
 
     image.src = `${path}${i}.png`;
-    image.classList.add("wall");
+    image.classList.add("hires");
+    image.classList.add("lores");
     image.classList.add("logo");
+    image.src = `${path}${i}.svg`;
+    image.classList.add("grub");
 
     mainDiv.appendChild(image);
   }
 };
 
-addImages(122, wallPath, wallDiv);
-addImages(7, logoPath, logoDiv);
+addImages(109, hiresPath, hiresDiv);
+addImages(22, lowresPath, lowresDiv);
+addImages(24, logoPath, logoDiv);
+addImages(6, grubPath, grubDiv);
