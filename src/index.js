4ff78bae855d560ee1891e616a215dbe4e25d7c9
zenobit <zenobit@disroot.org>
2025-12-19 02:47:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 let hiDiv = document.querySelector(".hiDiv");
 let hiPath = "assets/wallpapers-hires/";
+
 let lowDiv = document.querySelector(".lowDiv");
 let lowPath = "assets/wallpapers-lowres/";
+
 let logoDiv = document.querySelector(".logoDiv");
 let logoPath = "assets/logos/";
+
 let grubDiv = document.querySelector(".grubDiv");
 let grubPath = "assets/grub/inkspace-files/";
 
-addImages = (n, path, mainDiv) => {
+addImages = (n, path, mainDiv, extension) => {
   for (let i = 1; i <= n; i++) {
     const image = document.createElement("img");
-
-    image.src = `${path}${i}.png`;
-    image.classList.add("hires");
-    image.classList.add("lores");
-    image.classList.add("logo");
-    image.src = `${path}${i}.svg`;
-    image.classList.add("grub");
-
+    image.src = `${path}${String(i).padStart(3, '0')}.${extension}`;
     mainDiv.appendChild(image);
   }
 };
 
-addImages(109, hiresPath, hiresDiv);
-addImages(22, lowresPath, lowresDiv);
-addImages(24, logoPath, logoDiv);
-addImages(6, grubPath, grubDiv);
+addImages(105, hiPath, hiDiv, 'png');
+addImages(4, hiPath, hiDiv, 'jpg');
+addImages(22, lowPath, lowDiv, 'png');
+addImages(23, logoPath, logoDiv, 'png');
+addImages(2, logoPath, logoDiv, 'svg');
+addImages(6, grubPath, grubDiv, 'svg');
